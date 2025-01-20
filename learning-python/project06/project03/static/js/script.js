e35a912d5353672c45624f1b29f8f05df420d7a2
WIN-JSOA1D9C5C1\aw702 <aw7022@gmail.com>
2025-01-20 22:29:58 +0900

    feat: Add delayed mouse-following functionality to cursor

--- learning-python/project06/project03/static/js/script.js
+++ learning-python/project06/project03/static/js/script.js
@@ -1,8 +1,28 @@
-// script.js
 document.addEventListener('DOMContentLoaded', () => {
     const cursor = document.getElementById('cursor');
+
+    let cursorX = 0;
+    let cursorY = 0;
+    let targetX = 0;
+    let targetY = 0;
+
+    const speed = 0.1;
+
     document.addEventListener('mousemove', (event) => {
-        cursor.style.left = `${event.pageX}px`;
-        cursor.style.top = `${event.pageY}px`;
+        targetX = event.pageX;
+        targetY = event.pageY;
     });
+
+
+    function animate() {
+        cursorX += (targetX - cursorX) * speed;
+        cursorY += (targetY - cursorY) * speed;
+
+        cursor.style.left = `${cursorX + 10}px`;
+        cursor.style.top = `${cursorY + 10}px`;
+
+        requestAnimationFrame(animate);
+    }
+
+    animate();
 });
